ff3678d808f4821f7d1939dc49814dfde4acb478
akbar7701 <60427674+akbar7701@users.noreply.github.com>
2020-05-05 16:19:13 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,10 +1,8 @@
 // prettier-ignore
 export default [
-    { author: "Doimuu", quotes: "I don't know why, but i love you" },
-    { author: "Doiimuu", quotes: "Have a Nice Day zeyengg" },
-    { author: "Doiimuuu", quotes: "Hey! I luv uuuu! UwU" },
-    { author: "Ngga tau siapa hehe", quotes: "I love you." },
-    { author: "Aku", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "Doimuu", quotes: "I love you." },
-    { author: "Hanya orang biasa hehe", quotes: "I'm sorry for loving you." },
+    { author: "-Doiimuu", quotes: "I don't know why, but i love you" },
+    { author: "-Doiimuu", quotes: "Have a Nice Day zeyengg" },
+    { author: "-Doiimuu", quotes: "Hey! I luv uuuu! UwU" },
+    { author: "-Doiimuu", quotes: "I love you." },
+    { author: "-Doiimuu", quotes: "I may not with you everyday, but I love you everyday." },
 ];
