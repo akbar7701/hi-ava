154fe1c8ec90677f7256d8ca2634d4b002d4574b
akbar7701 <60427674+akbar7701@users.noreply.github.com>
2020-05-05 15:53:31 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -1,8 +1,8 @@
 // prettier-ignore
 export default [
-    { author: "avrill.v", quotes: "Whatever happens, keep breathing" },
-    { author: "antjedtta_", quotes: "Pam pam pararam ceklek jedar\nPararam ram parararam ram" },
-    { author: "standinshd", quotes: "Hey! I wuv chuuuu! UwU" },
+    { author: "Doimuu", quotes: "I don't know why, but i love you" },
+    { author: "Doiimuu", quotes: "Have a Nice Day zeyengg" },
+    { author: "standinshd", quotes: "Hey! I luv uuuu! UwU" },
     { author: "standinshd", quotes: "I love you." },
     { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
     { author: "standinshd", quotes: "I love you." },
