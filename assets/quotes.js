1b5f96d4e7ed9595e362705a89f006d71605fef1
akbar7701 <60427674+akbar7701@users.noreply.github.com>
2020-05-05 15:55:03 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -2,9 +2,9 @@
 export default [
     { author: "Doimuu", quotes: "I don't know why, but i love you" },
     { author: "Doiimuu", quotes: "Have a Nice Day zeyengg" },
-    { author: "standinshd", quotes: "Hey! I luv uuuu! UwU" },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I may not with you everyday, but I love you everyday." },
-    { author: "standinshd", quotes: "I love you." },
-    { author: "standinshd", quotes: "I'm sorry for loving you." },
+    { author: "Doiimuuu", quotes: "Hey! I luv uuuu! UwU" },
+    { author: "Ngga tau siapa hehe", quotes: "I love you." },
+    { author: "Aku", quotes: "I may not with you everyday, but I love you everyday." },
+    { author: "Doimuu", quotes: "I love you." },
+    { author: "Hanya orang biasa hehe", quotes: "I'm sorry for loving you." },
 ];
