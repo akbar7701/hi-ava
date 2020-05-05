9618215c7431c1747a15339f063372448587b125
akbar7701 <60427674+akbar7701@users.noreply.github.com>
2020-05-05 16:31:32 +0700

    Update quotes.js

--- assets/quotes.js
+++ assets/quotes.js
@@ -2,7 +2,5 @@
 export default [
     { author: "-Doiimuu", quotes: "I don't know why, but i love you" },
     { author: "-Doiimuu", quotes: "Have a Nice Day zeyengg" },
-    { author: "-Doiimuu", quotes: "Hey! I luv uuuu! UwU" },
-    { author: "-Doiimuu", quotes: "I love you." },
     { author: "-Doiimuu", quotes: "I may not with you everyday, but I love you everyday." },
 ];
